63b98ae5da4e43b35f6a7f087c0fd88a53899216
caongocthai <ngocthaicao@gmail.com>
2025-10-30 23:49:24 +0800

    Make message input textarea autofocus

--- src/components/thread/index.tsx
+++ src/components/thread/index.tsx
@@ -486,6 +486,7 @@ export function Thread() {
                         }}
                         placeholder="Type your message..."
                         className="field-sizing-content resize-none border-none bg-transparent p-3.5 pb-0 shadow-none ring-0 outline-none focus:ring-0 focus:outline-none"
+                        autoFocus
                       />
 
                       <div className="flex items-center gap-6 p-2 pt-4">
